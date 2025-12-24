aba971c83ceb230424daaf656f1755cc5cd232a6
Giorgi Kvashilava <gikvashilava@mod.local>
2025-12-24 11:10:24 +0400

    Add gitignore and update App files

--- src/App.jsx
+++ src/App.jsx
@@ -26,6 +26,34 @@ function App() {
 
 	return (
 		<div className='landing-page'>
+			<div className='background-text'>
+				<span className='bg-char'>t</span>
+				<span className='bg-char'>r</span>
+				<span className='bg-char'>a</span>
+				<span className='bg-char'>n</span>
+				<span className='bg-char'>s</span>
+				<span className='bg-char'> </span>
+				<span className='bg-char'>a</span>
+				<span className='bg-char'>t</span>
+				<span className='bg-char'>l</span>
+				<span className='bg-char'>a</span>
+				<span className='bg-char'>n</span>
+				<span className='bg-char'>t</span>
+				<span className='bg-char'>i</span>
+				<span className='bg-char'>c</span>
+				<span className='bg-char'> </span>
+				<span className='bg-char'>a</span>
+				<span className='bg-char'>u</span>
+				<span className='bg-char'>t</span>
+				<span className='bg-char'>o</span>
+				<span className='bg-char'>i</span>
+				<span className='bg-char'>m</span>
+				<span className='bg-char'>p</span>
+				<span className='bg-char'>o</span>
+				<span className='bg-char'>r</span>
+				<span className='bg-char'>t</span>
+			</div>
+
 			<div className='logo-container'>
 				<img src='/logo.png' alt='TAA Logo' className='logo-image' />
 			</div>
